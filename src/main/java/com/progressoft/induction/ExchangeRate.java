c932b6444cb2874d4ea933424cd9718c99525992
Ahmad Y. Saleh <ahmad.y.saleh@gmail.com>
2017-10-05 20:04:35 +0300

    used fake exchange rate service

--- src/main/java/com/progressoft/induction/ExchangeRate.java
+++ src/main/java/com/progressoft/induction/ExchangeRate.java
@@ -1,77 +1,77 @@
 package com.progressoft.induction;
 
-import com.google.gson.Gson;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Currency;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ExchangeRate {
 
+    private Map<Exchange, BigDecimal> exchangeRates = new HashMap<>();
     private final Currency fromCurrency;
     private final Currency toCurrency;
 
     public ExchangeRate(Currency fromCurrency, Currency toCurrency) {
         this.fromCurrency = fromCurrency;
         this.toCurrency = toCurrency;
+        addRate("JOD", "USD", "1.43");
+        addRate("USD", "JOD", "0.71");
+        addRate("EUR", "USD", "1.1");
+        addRate("USD", "EUR", "0.91");
+        addRate("USD", "THB", "0.5");
+        addRate("THB", "USD", "0.02");
+    }
+
+    private BigDecimal addRate(String fromCurrency, String toCurrency, String exchangeRate) {
+        return this.exchangeRates.put(new Exchange(Currency.getInstance(fromCurrency), Currency.getInstance(toCurrency)), new BigDecimal(exchangeRate));
     }
 
     public BigDecimal getRate() {
-        if(fromCurrency.getCurrencyCode().equals(toCurrency.getCurrencyCode())){
+        if (fromCurrency.getCurrencyCode().equals(toCurrency.getCurrencyCode())) {
             return BigDecimal.ONE;
         }
 
-        CurrencyExchangeInfo exchangeInfo = getExchangeInfo();
-        if(toCurrency.getCurrencyCode().equals(exchangeInfo.getBase())){
-            return BigDecimal.ONE.divide(getCurrencyRate(exchangeInfo, fromCurrency), 5, RoundingMode.CEILING);
-        }else{
-            return getCurrencyRate(exchangeInfo, toCurrency);
-        }
+        // usually exchange rate is retrieved from an external system
+        // this is a simulation for network delay
+        networkDelay();
+        return exchangeRates.get(new Exchange(fromCurrency, toCurrency));
     }
 
-    private String buildExchangeUrl(Currency fromCurrency, Currency toCurrency) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("http://api.fixer.io/latest?symbols=");
-        builder.append(fromCurrency.getCurrencyCode());
-        builder.append(",");
-        builder.append(toCurrency.getCurrencyCode());
-        return builder.toString();
-    }
-
-    private CurrencyExchangeInfo getExchangeInfo(){
+    private void networkDelay() {
         try {
-            URL url = new URL(buildExchangeUrl(fromCurrency, toCurrency));
-            URLConnection connection = url.openConnection();
-            String response = readInStreamAsString(connection);
-            Gson gson = new Gson();
-            return gson.fromJson(response, CurrencyExchangeInfo.class);
-        } catch (IOException e) {
-            throw new IllegalStateException("Error while retrieving currency exchange!", e);
+            Thread.sleep(ThreadLocalRandom.current().nextInt(50, 1000));
+        } catch (InterruptedException e) {
+            // ignore
         }
     }
 
-    private String readInStreamAsString(URLConnection connection) throws IOException {
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))){
-            String line;
-            StringBuilder builder = new StringBuilder();
-            while((line = reader.readLine()) != null){
-                builder.append(line);
-                builder.append(System.lineSeparator());
-            }
-            return builder.toString();
-        }
-    }
+    class Exchange {
+        Currency fromCurrency;
+        Currency toCurrency;
 
-    private BigDecimal getCurrencyRate(CurrencyExchangeInfo exchangeInfo, Currency currency) {
-        BigDecimal fxRate = exchangeInfo.getRates().get(currency.getCurrencyCode());
-        if(fxRate == null){
-            throw new IllegalArgumentException("currency " + currency.getCurrencyCode() + " is not supported!");
+        public Exchange(Currency fromCurrency, Currency toCurrency) {
+            this.fromCurrency = fromCurrency;
+            this.toCurrency = toCurrency;
         }
-        return fxRate;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Exchange exchange = (Exchange) o;
+
+            if (fromCurrency != null ? !fromCurrency.equals(exchange.fromCurrency) : exchange.fromCurrency != null)
+                return false;
+            return toCurrency != null ? toCurrency.equals(exchange.toCurrency) : exchange.toCurrency == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = fromCurrency != null ? fromCurrency.hashCode() : 0;
+            result = 31 * result + (toCurrency != null ? toCurrency.hashCode() : 0);
+            return result;
+        }
     }
 }
--- src/test/java/com/progressoft/induction/BankAccountTest.java
+++ src/test/java/com/progressoft/induction/BankAccountTest.java
@@ -134,4 +134,6 @@ public class BankAccountTest {
         }
         return amountList;
     }
+
+
 }
